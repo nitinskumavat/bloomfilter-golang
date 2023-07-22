2f5a71e6152709d25fe2b0b5f21444daf497a389
nitinskumavat <nitin.s.kumavat@gmail.com>
2023-07-22 21:21:23 +0000

    Reduced the size by factor of 8 by dividing the total size size by 8

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"hash"
-
+	"math/rand"
+"time"
 	"github.com/spaolacci/murmur3"
 )
 
@@ -11,7 +12,8 @@ var mHashser hash.Hash32
 var seed uint32
 
 func init() {
-	mHashser = murmur3.New32WithSeed(seed)
+  rand.Seed(time.Now().UnixNano())
+	mHashser = murmur3.New32WithSeed(rand.Uint32())
 }
 
 func murmurHash(key string, size uint32) int {
@@ -23,31 +25,35 @@ func murmurHash(key string, size uint32) int {
 }
 
 type Bloomfilter struct {
-	filter []bool
+	filter []uint8
 	size   uint32
 }
 
 func newBloomFilter(size uint32) Bloomfilter {
 	return Bloomfilter{
-		make([]bool, size),
+		make([]uint8, size/8),
 		size,
 	}
 }
 
 func (b *Bloomfilter) exist(key string) bool {
 	idx := murmurHash(key, b.size)
-	return b.filter[idx]
+	bytePosition := idx / 8
+	bitPosition := idx % 8
+	return (b.filter[bytePosition] & (1 << bitPosition)) > 0
 }
 
 func (b *Bloomfilter) add(key string) {
 	idx := murmurHash(key, b.size)
-	b.filter[idx] = true
+	fmt.Println(idx)
+	bytePosition := idx / 8
+	bitPosition := idx % 8
+	b.filter[bytePosition] = b.filter[bytePosition] | (1 << bitPosition)
 }
 
 func main() {
-	seed = 10
 	bloomFilter := newBloomFilter(16)
-	dataSet := []string{"a", "b", "c", "d", "e"}
+	dataSet := []string{"a", "b", "c", "d", "e","f","g","h"}
 	for _, key := range dataSet {
 		bloomFilter.add(key)
 	}
